b16572b17ab64fa58b4fce8cb5cfb7ea46e9f8ef
keneanung <keneanung@googlemail.com>
2022-01-01 16:48:32 +0100

    Fix?

--- danger/rules/CorrectBranch.ts
+++ danger/rules/CorrectBranch.ts
@@ -4,7 +4,7 @@ import { SanityCheckRule } from "../classes/Rule";
 export const checkCorrectBranch = new SanityCheckRule(
   async (danger: DangerDSLType) => {
     console.log(danger.github.pr.base.ref)
-    return danger.git.base === 'development'
+    return danger.github.pr.base.ref === 'development'
   },
   'The target branch is not `development`. Are you sure you are targeting the correct branch?'
 )
